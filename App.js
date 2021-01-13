03902a505a0b53ce0e1de11a842c8310cbfd5838
alestor123 <alestoraldous@gmail.com>
2021-01-13 20:40:25 +0530

    chalk

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+require('dotenv').config()
 var app = require('express')(),
 {dialog} = require('electron'),
-options = require('minimist')(process.argv.slice(2));
+options = require('minimist')(process.argv.slice(2)),
+port = options.p || options.port || 3000;
+
+app.listen(port, () => console.log(chalk.greenBright(`Server running at ${port}`)))
