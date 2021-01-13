c7a9ec2b3279675655d7c627d7e7cabbd159748f
alestor123 <alestoraldous@gmail.com>
2021-01-13 20:43:59 +0530

    added home route

--- App.js
+++ App.js
@@ -5,6 +5,12 @@ var app = require('express')(),
 {dialog} = require('electron'),
 chalk = require('chalk'),
 options = require('minimist')(process.argv.slice(2)),
+pck = require('./package.json'),
 port = options.p || options.port || 3000;
-
+app.get('/github', (req, res) => {
+    res.redirect(pck.homepage)
+})
+app.get('/', (req, res) => {
+    res.json({popup:'service'})
+})
 app.listen(port, () => console.log(chalk.greenBright(`Server running at ${port}`)))
